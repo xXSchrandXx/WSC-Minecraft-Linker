ad23981816c28d65aaedebd15b26ba1b9af89592
xXSchrandXx <marvinschrand@yahoo.de>
2022-09-14 17:30:17 +0200

    Fix listener

--- src/main/java/de/xxschrandxx/wsc/wsclinker/bukkit/listener/WSCBridgePluginReloadListenerBukkit.java
+++ src/main/java/de/xxschrandxx/wsc/wsclinker/bukkit/listener/WSCBridgePluginReloadListenerBukkit.java
@@ -3,13 +3,14 @@ package de.xxschrandxx.wsc.wsclinker.bukkit.listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import de.xxschrandxx.wsc.wscbridge.bukkit.api.event.WSCBridgePluginReloadEventBukkit;
 import de.xxschrandxx.wsc.wsclinker.bukkit.MinecraftLinkerBukkit;
 import de.xxschrandxx.wsc.wsclinker.bukkit.api.event.WSCLinkerPluginReloadEventBukkit;
 import de.xxschrandxx.wsc.wsclinker.core.MinecraftLinkerVars;
 
 public class WSCBridgePluginReloadListenerBukkit implements Listener {
     @EventHandler
-    public void onPluginReload(WSCLinkerPluginReloadEventBukkit event) {
+    public void onPluginReload(WSCBridgePluginReloadEventBukkit event) {
         MinecraftLinkerBukkit instance = MinecraftLinkerBukkit.getInstance();
         String apiStart = instance.getConfiguration().getString(MinecraftLinkerVars.Configuration.LangCmdReloadAPIStart);
         event.getSender().sendMessage(apiStart);
